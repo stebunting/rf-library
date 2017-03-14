1c1269175bcdbb68d4c1c6ed4f8f48b92856c225
Ste Bunting <stebunting@gmail.com>
2017-03-14 11:26:55 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,16 @@
+# User Settings
+initialised = False
+forename = 'Stephen'
+surname = 'Bunting'
+defaultSourceLocation = '/Volumes/USER/PSA/TRACES'
+defaultLibraryLocation = '/Users/Ste/Documents/Audio/RF/Scans'
+defaultDateFormat = 'yyyy-dd-mm'
+lowFreqLimit = 470
+highFreqLimit = 890
+settingsLength = 279
+
+# Continuity Settings
+defaultVenue = 'Sentrum Scene'
+defaultTown = 'Oslo'
+defaultCountry = 'Norway'
+defaultDelete = False
