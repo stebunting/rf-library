6c2a501e892d86eca1b6db4a9050118a4b1e520e
Stephen Bunting <stebunting@gmail.com>
2017-03-17 19:49:49 +0100

    Made output directory truncate when it gets too big

--- src/setup.py
+++ src/setup.py
@@ -5,21 +5,40 @@ Usage:
     python setup.py py2app
 """
 
+import sys
 from setuptools import setup
 import data
+
+if sys.platform == 'win32':
+    import ez_setup
+    ez_setup.use_setuptools()
+
+mainscript = 'rflibrary.py'
 
 APP = ['rflibrary']
 APP_NAME = 'RFLibrary'
 DATA_FILES = ['data.py', 'settings.py', 'gui.ico']
-OPTIONS = {
-    'includes':'tkinter',
-	'iconfile':'rflibrary.icns',
-	'plist': {
-	'CFBundleName': APP_NAME,
-    'CFBundleDisplayName': APP_NAME,
-    'CFBundleVersion': data.version,
-    'CFBundleShortVersionString': data.version, }
-    }
+if sys.platform == 'darwin':
+    OPTIONS = {
+        'includes':'tkinter',
+        'iconfile':'rflibrary.icns',
+        'plist': {
+        'CFBundleName': APP_NAME,
+        'CFBundleDisplayName': APP_NAME,
+        'CFBundleVersion': data.version,
+        'CFBundleShortVersionString': data.version, }
+        }
+elif sys.platform == 'win32':
+     extra_options = dict(
+         setup_requires=['py2exe'],
+         app=[mainscript],
+     )
+else:
+     extra_options = dict(
+         # Normally unix-like platforms will use "setup.py install"
+         # and install the main script as such
+         scripts=[mainscript],
+     )
 
 setup(
     app = APP,
@@ -27,3 +46,4 @@ setup(
     options = {'py2app': OPTIONS},
     setup_requires = ['py2app'],
 )
+
